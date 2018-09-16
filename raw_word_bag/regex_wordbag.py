c14570dcf43ddbc1c0c6dba292950712ba6ceb7c
Cherrybruin <liyunkai@foxmail.com>
2018-09-16 10:18:50 +0800

    tokenization word with regax

--- raw_word_bag/regex_wordbag.py
+++ raw_word_bag/regex_wordbag.py
@@ -14,5 +14,48 @@ dir_name = '20news'
 a_sub_dir = listdir(dir_name)[0]
 a_file = listdir(path.join(dir_name,a_sub_dir))[0]
 a_file = path.join(dir_name, a_sub_dir, a_file)
-with open(a_file, 'rb') as fp:
-    print(fp.read())
+def raw_token_set(filename:str):
+    '''
+        param:
+        :: filename: a file path of a text file
+        :: returntype: set
+            return the set of all the words of the text
+    '''
+    wordbag = set()
+    word = re.compile(r'[a-zA-Z]+-?[a-zA-Z]+')
+    num = re.compile(r'\d+')
+    with open(filename, 'rb') as fp:
+        for line in fp:
+            temp = str(line).strip().lower()
+            if not temp:
+                continue
+            # print(temp)
+            for i in re.findall(word, temp):
+                wordbag.add(i.strip())
+            for i in re.findall(num, temp):
+                wordbag.add(i.strip())
+    return wordbag
+
+def raw_token_list(filename:str):
+    '''
+        param:
+        :: filename: a file path of a text file
+        :: returntype: list
+            return the list of all the words of the text
+    '''
+    wordbag = list()
+    word = re.compile(r'[a-zA-Z]+-?[a-zA-Z]+')
+    num = re.compile(r'\d+')
+    with open(filename, 'rb') as fp:
+        for line in fp:
+            temp = str(line).strip().lower()
+            if not temp:
+                continue
+            # print(temp)
+            for i in re.findall(word, temp):
+                wordbag.append(i.strip())
+            for i in re.findall(num, temp):
+                wordbag.append(i.strip())
+    return wordbag
+
+print(raw_token_list(a_file))
